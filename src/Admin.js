078665cbf4f0001a56ec916e672fe98c5716f046
ekibru <kibru4204@gmail.com>
2023-07-11 14:22:26 -0500

    first commit

---
+++ src/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Admin() {
+    return (
+        <div className="App">
+        <h1>Admin page</h1>
+        
+    
+        </div>
+    );
+    }
